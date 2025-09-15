587ff91e75cbf3ff10f1f3db079ce2565adf2301
PiettroXavier <piettroxavier2016@gmail.com>
2025-09-15 09:35:28 -0300

    Add files via upload
    Signed-off-by: PiettroXavier <piettroxavier2016@gmail.com>

---
+++ PiettroXavierDeSouza_Biblioteca/biblioteca.js
@@ -0,0 +1,327 @@
+const livrosfuvest = {
+    /*Objeto com todos os livros de leitura obrigatória entre 2026 a 2029*/
+
+    OpusculoHumanitario:[{
+        Titulo:"Opúsculo Humanitário", 
+        Autor:"Nisia Floresta",
+        Genero:"Literário",
+        Editora:"Senado Federal",
+        Ano: 1853,
+        Descricao: "O Opúsculo Humanitário é uma obra escrita por Nísia Floresta em 1853, que aborda temas relacionados aos direitos das mulheres e à educação. Nísia Floresta foi uma das primeiras feministas do Brasil e sua obra é considerada pioneira na luta pelos direitos das mulheres no país.",
+
+        Capa: 'img/OpusculoHumanitario.webp',
+        Online:'https://www2.senado.leg.br/bdsf/bitstream/handle/id/562126/Opusculo_humanitario.pdf',
+        Audio:'aud/OpusculoHumanitario.mp3'
+    }],
+
+    Nebulosas:[{
+        Titulo:"Nebulosas",
+        Autor:"Narcisa Amália",
+        Genero:"Poesia",
+        Editora:"Câmara Brasileira do Livro",
+        Ano: 1872,
+        Descricao: "Nebulosas é uma coletânea de poemas escritos por Narcisa Amália, uma das primeiras poetisas brasileiras. Publicada em 1872, a obra aborda temas como a condição feminina, a liberdade e a justiça social, refletindo as preocupações da autora com os direitos das mulheres e a igualdade.",
+
+        Capa: 'img/Nebulosas.webp',
+        Online:'https://fliphtml5.com/dyomx/gzck/Nebulosas_-_Narcisa_Amália/'
+        ,Audio:'aud/Nebulosas.mp3'
+    }],
+
+    MemoriasDeMartha:[{
+        Titulo:"Memórias de Martha",
+        Autor:"Julia Lopes de Almeida",
+        Genero:"Romance",
+        Editora:"Casa Durski",
+        Ano: 1899,
+        Descricao: "Memórias de Martha é um romance escrito por Júlia Lopes de Almeida, publicado em 1899. A obra narra a vida de Martha, uma jovem que enfrenta os desafios da sociedade patriarcal do século XIX. O livro aborda temas como a educação das mulheres, o casamento e a busca pela independência feminina.",
+
+        Capa: 'img/MemoriasDeMartha.jpg',
+        Online:'https://digital.bbm.usp.br/view/?45000009365&bbm/7037#page/6/mode/2up'
+        ,Audio:'aud/MamoriasDeMartha.mp3'
+    }],
+
+    CaminhoDasPedras:[{
+        Titulo:"Caminho das Pedras",
+        Autor:"Rachel de Queiroz",
+        Genero:"Romance",
+        Editora:"José Olympio LTDA.",
+        Ano: 1937,
+        Descricao: "Caminho das Pedras é um romance escrito por Rachel de Queiroz, publicado em 1937. A obra retrata a vida de uma mulher nordestina que luta contra as adversidades impostas pela seca e pela pobreza. O livro aborda temas como a resistência feminina, a luta pela sobrevivência e a busca por uma vida melhor.",
+
+        Capa: 'img/CaminhoDasPedras.jpg',
+        Online:'https://pt.scribd.com/document/868154261/Caminho-de-Pedras-Rachel-de-Queiroz'
+        ,Audio:'aud/CaminhoDasPedras.mp3'
+    }],
+
+    OCristoCigano:[{
+        Titulo:"O Cristo Cigano",
+        Autor:"Sophia de Mello Breyner Andresen",
+        Genero:"Romance",
+        Editora:"Companhia Das Letras",
+        Ano: 1961,
+        Descricao: "O Cristo Cigano é um romance escrito por Sophia de Mello Breyner Andresen, publicado em 1961. A obra narra a história de um jovem que busca compreender sua identidade e seu lugar no mundo. O livro aborda temas como a liberdade, a espiritualidade e a busca por sentido na vida.",
+        
+        Capa: 'img/OCristoCigano_Geografia.jpg',
+        Online:'https://pt.scribd.com/document/837241027/O-Cristo-Cigano-e-Geografia-Sophia-de-Mello-Breyner-Andresen'
+        ,Audio:'aud/OCristoCigano.mp3'
+    }],
+
+    AsMeninas:[{
+        Titulo:"As Meninas",
+        Autor:"Lygia Fagundes Telles",
+        Genero:"Contos",
+        Editora:"Companhia das Letras",
+        Ano: 1973,
+        Descricao: "As Meninas é uma coletânea de contos escrita por Lygia Fagundes Telles, publicada em 1973. A obra reúne histórias que exploram a complexidade das relações humanas, especialmente entre mulheres. Os contos abordam temas como a solidão, a busca por identidade e a luta contra as opressões sociais.",
+
+        Capa: 'img/AsMeninas.jpg',
+        Online:'https://caleufscar.wordpress.com/wp-content/uploads/2016/07/as-meninas-lygia-fagundes-telles.pdf'
+        ,Audio:'aud/AsMeninas.mp3'
+    }],
+    
+    BaladaDeAmorAoVento:[{
+        Titulo:"Balada de Amor ao Vento",
+        Autor:"Paula Chiziane",
+        Genero:"Romance",
+        Editora:"Editoral Caminho",
+        Ano: 1990,
+        Descricao: "Balada de Amor ao Vento é um romance escrito por Ana Miranda, publicado em 1990. A obra narra a história de uma mulher que busca reencontrar suas raízes e sua identidade cultural. O livro aborda temas como a memória, a história e a luta pela preservação da cultura indígena.",
+
+        Capa: 'img/BaladaDeAmorAoVento.jpg',
+        Online:'https://www.kufunda.net/publicdocs/Paulina_Chiziane_-_Balada_de_Amor_ao_Vento.pdf.pdf.pdf'
+        ,Audio:'aud/BaladaDeAmorAoVento.mp3'
+    }],
+
+    CancaoParaNinarMeninoGrande:[{
+        Titulo:"Canção para Ninar Menino Grande",
+        Autor:"Conceição Evaristo",
+        Genero:"Contos",
+        Editora:"UniPalmares",
+        Ano: 2018,
+        Descricao: "Canção para Ninar Menino Grande é uma coletânea de contos escrita por Conceição Evaristo, publicada em 2018. A obra reúne histórias que exploram a experiência negra no Brasil, abordando temas como a identidade, a resistência e a luta contra o racismo. Os contos refletem a vivência da autora e sua perspectiva sobre a sociedade brasileira.",
+
+        Capa: 'img/CancaoParaNinarMeninoGrande.jpg',
+        Online:'https://pt.scribd.com/document/869945392/Cancao-para-ninar-menino-grande'
+        ,Audio:'aud/CancaoParaNinarMeninoGrande.mp3'
+    }],
+
+    AVisaoDasPlantas:[{
+        Titulo:"A Visão das Plantas",
+        Autor:"Djaimilia Pereira de Almeida",
+        Genero:"Romance",
+        Editora:"Editora Todavia",
+        Ano: 2019,
+        Descricao: "A Visão das Plantas é um romance escrito por Djaimilia Pereira de Almeida, publicado em 2019. A obra narra a história de uma mulher que busca compreender sua identidade e suas raízes culturais. O livro aborda temas como a memória, a história e a luta pela preservação da cultura africana.",
+
+        Capa: 'img/AVisaoDasPlantas.jpg',
+        Online:'https://pt.scribd.com/document/546691486/A-visao-das-plantas-Djaimilia-Pereira-de-Almeida'
+        ,Audio:'aud/AVisaoDasPlantas.mp3'
+    }],
+
+
+    /*2027*/ 
+
+    APaixaoSegundoGH:[{
+        Titulo:"A Paixão Segundo G.H.",
+        Autor:"Clarice Lispector",
+        Genero:"Romance",
+        Editora:"Rocco",
+        Ano: 1964,
+        Descricao: "A Paixão Segundo G.H. é um romance escrito por Clarice Lispector, publicado em 1964. A obra narra a experiência de uma mulher que enfrenta uma crise existencial após um encontro inesperado. O livro aborda temas como a identidade, a solidão e a busca por sentido na vida.",
+
+        Capa: 'img/APaixaoSegundoGH.jpg',
+        Online:'https://www.atelierpaulista.com/wp-content/uploads/2015/07/apaixaosegundogh.pdf'
+        ,Audio:'aud/APaixaoSegundoGH.mp3'
+    }],
+
+    Geografia:[{
+        Titulo:"Geografia",
+        Autor:"Sophia de Mello Breyner Andresen",
+        Genero:"Romance",
+        Editora:"Companhia Das Letras",
+        Ano: 1967,
+        Descricao: "Geografia é um romance escrito por Sophia de Mello Breyner Andresen, publicado em 1967. A obra narra a história de uma jovem que busca compreender sua identidade e seu lugar no mundo. O livro aborda temas como a liberdade, a espiritualidade e a busca por sentido na vida.",
+        
+        Capa: 'img/OCristoCigano_Geografia.jpg',
+        Online:'https://pt.scribd.com/document/837241027/O-Cristo-Cigano-e-Geografia-Sophia-de-Mello-Breyner-Andresen'
+        ,Audio:'aud/Geografia.mp3'
+    }],
+
+
+    /*2028*/
+
+    ConselhosAMinhaFilha:[{
+        Titulo:"Conselhos a Minha Filha",
+        Autor:"Nísia Floresta",
+        Genero:"Literário",
+        Editora:"Typographia de F. de Paula Brito.",
+        Ano: 1842,
+        Descricao: "Conselhos a Minha Filha é uma obra escrita por Nísia Floresta em 1842, que aborda temas relacionados aos direitos das mulheres e à educação. Nísia Floresta foi uma das primeiras feministas do Brasil e sua obra é considerada pioneira na luta pelos direitos das mulheres no país.",
+
+        Capa: 'img/ConselhosAMinhaFilha.jpg',
+        Online:'https://books.google.pt/books?id=c_ZCAQAAMAAJ&printsec=frontcover&hl=pt-BR&source=gbs_ge_summary_r&cad=0#v=onepage&q&f=false',
+        Audio:'aud/ConselhosAMinhaFilha.mp3'
+    }],
+
+    JoaoMiguel:[{
+        Titulo:"João Miguel",
+        Autor:"Rachel de Queiroz",
+        Genero:"Romance",
+        Editora:"José Olympio LTDA.",
+        Ano: 1932,
+        Descricao: "João Miguel é um romance escrito por Rachel de Queiroz, publicado em 1932. A obra retrata a vida de um jovem nordestino que enfrenta as adversidades impostas pela seca e pela pobreza. O livro aborda temas como a luta pela sobrevivência e a busca por uma vida melhor.",
+
+        Capa: 'img/JoaoMiguel.jpg',
+        Online:'https://www.academia.edu/36064254/João_miguel'
+        ,Audio:'aud/JoaoMiguel.mp3'
+    }],
+
+
+    /*2029*/
+
+    DomCasmurro:[{
+        Titulo:"Dom Casmurro",
+        Autor:"Machado de Assis",
+        Genero:"Romance",
+        Editora:"Edições Câmara",
+        Ano: 1899,
+        Descricao: "Dom Casmurro é um romance escrito por Machado de Assis, publicado em 1899. A obra narra a história de Bento Santiago, um homem que relembra sua juventude e seu amor por Capitu. O livro aborda temas como a dúvida, a traição e a complexidade das relações humanas.",
+
+        Capa: 'img/DomCasmurro.webp',
+        Online:'https://ddcus.org/pdf/summer_reading/11th_grade/Dom_Casmurro-Machado_de_Assis.pdf'
+        ,Audio:'aud/DomCasmurro.mp3'
+    }],
+
+    NosMatamosOCaoTinhoso:[{
+        Titulo:"Nós Matamos o Cão Tinhoso",
+        Autor:"Luís Bernardo Honwana",
+        Genero:"Contos",
+        Editora:"Kapulana",
+        Ano: 1964,
+        Descricao: "Nós Matamos o Cão Tinhoso é uma coletânea de contos escrita por Luís Bernardo Honwana, publicada em 1964. A obra reúne histórias que exploram a experiência africana, abordando temas como a colonização, a identidade e a luta pela liberdade. Os contos refletem a vivência do autor e sua perspectiva sobre a sociedade moçambicana.",
+
+        Capa: 'img/NosMatamosOCaoTinhoso.jpg',
+        Online:'https://pt.slideshare.net/slideshow/honwana-luis-bernardo-nos-matamos-o-cao-tinhoso/267142597'
+        ,Audio:'aud/NosMatamosOCaoTinhoso.mp3'
+    }],
+
+    IncidenteEmAntares:[{
+        Titulo:"Incidente em Antares",
+        Autor:"Érico Veríssimo",
+        Genero:"Romance",
+        Editora:"Editora Globo",
+        Ano: 1970,
+        Descricao: "Incidente em Antares é um romance escrito por Érico Veríssimo, publicado em 1970. A obra narra a história de uma pequena cidade fictícia chamada Antares, onde os mortos voltam à vida para reivindicar seus direitos. O livro aborda temas como a justiça social, a corrupção e a luta pela igualdade.",
+
+        Capa: 'img/IncidenteEmAntares.jpg',
+        Online:'https://kbook.com.br/wp-content/files_mf/erico_incidente_em_antares.pdf'
+        ,Audio:'aud/IncidenteEmAntares.mp3'
+    }],
+
+  };
+
+
+/*Função gera pop-up com todas as informações provenientes do objeto ao clicar na capa do livro*/
+ function livro(name){
+    const conteiner = document.getElementById("PopUp");
+
+    conteiner.innerHTML = `
+    <div id="overlay">
+      <div id="popup">
+        <span id="btnFechar"></span>
+
+        <div>
+        <a href="${livrosfuvest[name][0].Online}" target="_blank"><img src="${livrosfuvest[name][0].Capa}" class="capa"></a>
+        <br><br>
+        <a href="${livrosfuvest[name][0].Online}" target="_blank"><button>Leia Online Grátis!</button></a>
+        </div>
+
+        <div class="linha-vertical"></div>
+
+        <div>
+        <h1>${livrosfuvest[name][0].Titulo}</h1>
+        <hr>
+        <h2>Autor(a): ${livrosfuvest[name][0].Autor}</h2>
+        <hr>
+        <h2>Gênero: ${livrosfuvest[name][0].Genero}</h2>
+        <hr>
+        <h2>Editora: ${livrosfuvest[name][0].Editora}</h2>
+        <hr>
+        <h2>Ano de Publicação:  ${livrosfuvest[name][0].Ano}</h2>
+        <hr>
+        <h2>${livrosfuvest[name][0].Descricao}</h2>
+        <hr>
+        <audio src="${livrosfuvest[name][0].Audio}" controls></audio>
+
+        <img src="img/close.png" class="close" onclick="fecharPopup(overlay, popup)">
+
+        </div>
+
+      </div>
+    </div>
+  `;
+  
+/*Função que fecha pop-up ao clicar em qualuqre local da tela ou no botão de fechar*/
+const overlay = document.getElementById("overlay");
+const popup = document.getElementById("popup");
+const btnFechar = document.getElementById("btnFechar");
+
+overlay.style.display = "flex";
+
+  setTimeout(() => {
+    popup.classList.add("show");
+  }, 10);
+
+  overlay.addEventListener("click", (event) => {
+    if (event.target === overlay) {
+      fecharPopup(overlay, popup);
+    }
+  });
+}
+
+/* Função que anima fechamento do pop-up */
+function fecharPopup(overlay, popup) {
+  popup.classList.remove("show");
+  setTimeout(() => {
+    overlay.style.display = "none";
+  }, 300); // mesmo tempo do transition      
+}
+
+/*Function que desativa hover de imagens para evitar conflito com pop-up*/
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("img").forEach(img => {
+        img.addEventListener("click", () => {
+            img.setAttribute("data-no-hover", "true");
+
+           
+            const reset = () => {
+                img.removeAttribute("data-no-hover");
+                document.removeEventListener("click", reset, true);
+            };
+
+            
+            document.addEventListener("click", reset, true);
+        });
+    });
+});
+
+/*Função que anima barra de navegação, fazendo-a sumir e reapárecer*/
+document.addEventListener("DOMContentLoaded", () => {
+    const navbar = document.querySelector('.navbar');
+
+    document.addEventListener('mousemove', (e) => {
+        // mostra a navbar se o mouse estiver perto do topo
+        if (e.clientY < 50 || window.scrollY === 0) {
+            navbar.style.top = "0";
+        } else {
+            navbar.style.top = "-100px"; // esconde a navbar
+        }
+    });
+
+    window.addEventListener('scroll', () => {
+        // se estiver no topo, mostra
+        if (window.scrollY === 0) {
+            navbar.style.top = "0";
+        }
+    });
+});
