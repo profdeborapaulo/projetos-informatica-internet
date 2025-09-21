cddb420d3fd5de970bbe050c4d7e5ab54070accd
PiettroXavier <piettroxavier2016@gmail.com>
2025-09-21 11:42:14 -0300

    Update biblioteca.js
    Signed-off-by: PiettroXavier <piettroxavier2016@gmail.com>

--- PiettroXavierDeSouza_Biblioteca/biblioteca.js
+++ PiettroXavierDeSouza_Biblioteca/biblioteca.js
@@ -7,7 +7,7 @@ const livrosfuvest = {
         Genero:"Literário",
         Editora:"Senado Federal",
         Ano: 1853,
-        Descricao: "O Opúsculo Humanitário é uma obra escrita por Nísia Floresta em 1853, que aborda temas relacionados aos direitos das mulheres e à educação. Nísia Floresta foi uma das primeiras feministas do Brasil e sua obra é considerada pioneira na luta pelos direitos das mulheres no país.",
+        Descricao: "Opúsculo Humanitário, terceiro volume da Coleção Escritoras do Brasil, é uma coletânea de 62 artigos de Nísia Floresta, publicada em 1853. Parte desses artigos já havia sido publicada no Diário do Rio de Janeiro um ano antes e, depois da publicação da coletânea, no periódico O Liberal, desta vez na íntegra. Os temas dos artigos giram em torno da condição feminina e, principalmente, da importância da educação da mulher, vista pela autora como condição essencial para a superação da inferioridade feminina e para o progresso social.",
 
         Capa: 'img/OpusculoHumanitario.webp',
         Online:'https://www2.senado.leg.br/bdsf/bitstream/handle/id/562126/Opusculo_humanitario.pdf',
@@ -20,7 +20,7 @@ const livrosfuvest = {
         Genero:"Poesia",
         Editora:"Câmara Brasileira do Livro",
         Ano: 1872,
-        Descricao: "Nebulosas é uma coletânea de poemas escritos por Narcisa Amália, uma das primeiras poetisas brasileiras. Publicada em 1872, a obra aborda temas como a condição feminina, a liberdade e a justiça social, refletindo as preocupações da autora com os direitos das mulheres e a igualdade.",
+        Descricao: "Nebulosas é a única obra publicada por Narcisa Amália, lançada em 1872 quando ela tinha apenas 20 anos. A coletânea reúne 44 poemas que abordam temas como a condição feminina, a natureza e questões sociais, refletindo uma sensibilidade crítica e engajada para a época. A autora, poetisa, jornalista e ativista social, se destacou por sua postura ousada e pioneira, sendo reconhecida por figuras como Machado de Assis e Dom Pedro II. (Não Oficial)",
 
         Capa: 'img/Nebulosas.webp',
         Online:'https://fliphtml5.com/dyomx/gzck/Nebulosas_-_Narcisa_Amália/'
@@ -33,7 +33,7 @@ const livrosfuvest = {
         Genero:"Romance",
         Editora:"Casa Durski",
         Ano: 1899,
-        Descricao: "Memórias de Martha é um romance escrito por Júlia Lopes de Almeida, publicado em 1899. A obra narra a vida de Martha, uma jovem que enfrenta os desafios da sociedade patriarcal do século XIX. O livro aborda temas como a educação das mulheres, o casamento e a busca pela independência feminina.",
+        Descricao: "Memórias de Martha é o primeiro romance de Júlia Lopes de Almeida, publicado em 1888 como folhetim na Tribuna Liberal do Rio de Janeiro e, posteriormente, em formato de livro pela Casa Durski em 1899. A narrativa é conduzida em primeira pessoa pela protagonista Martha, que relembra sua infância e juventude em um cortiço no Rio de Janeiro. A obra aborda temas como a condição feminina, a importância da educação e a luta por uma vida melhor, refletindo as desigualdades sociais da época. (Não Oficial)",
 
         Capa: 'img/MemoriasDeMartha.jpg',
         Online:'https://digital.bbm.usp.br/view/?45000009365&bbm/7037#page/6/mode/2up'
@@ -46,7 +46,7 @@ const livrosfuvest = {
         Genero:"Romance",
         Editora:"José Olympio LTDA.",
         Ano: 1937,
-        Descricao: "Caminho das Pedras é um romance escrito por Rachel de Queiroz, publicado em 1937. A obra retrata a vida de uma mulher nordestina que luta contra as adversidades impostas pela seca e pela pobreza. O livro aborda temas como a resistência feminina, a luta pela sobrevivência e a busca por uma vida melhor.",
+        Descricao: "Caminho de Pedras é o terceiro romance de Rachel de Queiroz, publicado em 1937. A história se passa em Fortaleza durante a Era Vargas e acompanha a vida de Noemi, uma mulher casada com João Jaques, um ex-militante comunista agora desencantado. Noemi vive um casamento sem amor e, ao conhecer Roberto, um jovem idealista envolvido com o movimento de esquerda, desenvolve uma relação amorosa intensa e conflitante. A obra aborda temas como a autonomia feminina, a moralidade, a política e os dilemas éticos enfrentados pelos personagens em uma sociedade em transição. (Não Oficial)",
 
         Capa: 'img/CaminhoDasPedras.jpg',
         Online:'https://pt.scribd.com/document/868154261/Caminho-de-Pedras-Rachel-de-Queiroz'
@@ -59,7 +59,7 @@ const livrosfuvest = {
         Genero:"Romance",
         Editora:"Companhia Das Letras",
         Ano: 1961,
-        Descricao: "O Cristo Cigano é um romance escrito por Sophia de Mello Breyner Andresen, publicado em 1961. A obra narra a história de um jovem que busca compreender sua identidade e seu lugar no mundo. O livro aborda temas como a liberdade, a espiritualidade e a busca por sentido na vida.",
+        Descricao: "O Cristo Cigano é uma obra poética publicada em 1961 que narra, por meio de doze poemas, a história de um escultor que mata um homem cigano para usá-lo como modelo para uma escultura de Cristo. A obra explora temas como paixão, amor e morte, e é reconhecida por sua estrutura poética única e pela profundidade emocional que transmite. (Não Oficial)",
         
         Capa: 'img/OCristoCigano_Geografia.jpg',
         Online:'https://pt.scribd.com/document/837241027/O-Cristo-Cigano-e-Geografia-Sophia-de-Mello-Breyner-Andresen'
@@ -72,7 +72,7 @@ const livrosfuvest = {
         Genero:"Contos",
         Editora:"Companhia das Letras",
         Ano: 1973,
-        Descricao: "As Meninas é uma coletânea de contos escrita por Lygia Fagundes Telles, publicada em 1973. A obra reúne histórias que exploram a complexidade das relações humanas, especialmente entre mulheres. Os contos abordam temas como a solidão, a busca por identidade e a luta contra as opressões sociais.",
+        Descricao: "Num pensionato para moças, as universitárias Lorena, Lia e Ana Clara iniciam a vida adulta num mundo conturbado por rápidas transformações.",
 
         Capa: 'img/AsMeninas.jpg',
         Online:'https://caleufscar.wordpress.com/wp-content/uploads/2016/07/as-meninas-lygia-fagundes-telles.pdf'
@@ -85,7 +85,7 @@ const livrosfuvest = {
         Genero:"Romance",
         Editora:"Editoral Caminho",
         Ano: 1990,
-        Descricao: "Balada de Amor ao Vento é um romance escrito por Ana Miranda, publicado em 1990. A obra narra a história de uma mulher que busca reencontrar suas raízes e sua identidade cultural. O livro aborda temas como a memória, a história e a luta pela preservação da cultura indígena.",
+        Descricao: "Balada de Amor ao Vento é o primeiro romance de Paulina Chiziane, publicado em 1990. A história acompanha a trajetória de Sarnau, uma mulher moçambicana que narra suas lembranças de juventude em Mambone, onde viveu um intenso e breve romance com Mwando. Apesar de sua paixão, Mwando se casa com outra mulher escolhida por sua família, deixando Sarnau desiludida. Ela então casa-se com Nguila, um homem polígamo que se torna o futuro rei de sua terra. A obra aborda temas como amor, desilusão, poligamia e a luta pela sobrevivência em uma sociedade marcada por tradições e desafios culturais. (Não Oficial)",
 
         Capa: 'img/BaladaDeAmorAoVento.jpg',
         Online:'https://www.kufunda.net/publicdocs/Paulina_Chiziane_-_Balada_de_Amor_ao_Vento.pdf.pdf.pdf'
@@ -98,7 +98,7 @@ const livrosfuvest = {
         Genero:"Contos",
         Editora:"UniPalmares",
         Ano: 2018,
-        Descricao: "Canção para Ninar Menino Grande é uma coletânea de contos escrita por Conceição Evaristo, publicada em 2018. A obra reúne histórias que exploram a experiência negra no Brasil, abordando temas como a identidade, a resistência e a luta contra o racismo. Os contos refletem a vivência da autora e sua perspectiva sobre a sociedade brasileira.",
+        Descricao: "Canção para ninar menino grande é uma novela publicada originalmente em 2018 pela Editora Unipalmares, escrita por Conceição Evaristo, que explora profundamente questões de gênero, raça e relações afetivas. A história gira em torno de Fio Jasmin, um atraente homem negro que trabalha como ajudante de maquinista e se relaciona com diversas mulheres. A obra apresenta uma narrativa em primeira pessoa plural, criando uma proximidade especial com as personagens, e utiliza elementos fantásticos sutis que permeiam o cotidiano. A autora aborda temas como masculinidade negra, dororidade (dor específica vivenciada por mulheres negras), poder feminino e sororidade, inspirados na tradição iorubá, onde o reconhecimento mútuo é fundamental para a existência. (Não Oficial)",
 
         Capa: 'img/CancaoParaNinarMeninoGrande.jpg',
         Online:'https://pt.scribd.com/document/869945392/Cancao-para-ninar-menino-grande'
@@ -111,7 +111,7 @@ const livrosfuvest = {
         Genero:"Romance",
         Editora:"Editora Todavia",
         Ano: 2019,
-        Descricao: "A Visão das Plantas é um romance escrito por Djaimilia Pereira de Almeida, publicado em 2019. A obra narra a história de uma mulher que busca compreender sua identidade e suas raízes culturais. O livro aborda temas como a memória, a história e a luta pela preservação da cultura africana.",
+        Descricao: "Esta é a história de Celestino, um homem cujo passado de brutalidade e violência assombrosas é substituído, no crepúsculo da vida, por um amor delicado e cuidadoso pelas plantas de seu jardim. De volta a Portugal, e com a consciência pesada pelas monstruosidades que cometeu, o capitão retorna à casa de sua infância. Na vizinhança, as pessoas conhecem seus malfeitos, então poucos se atrevem a se aproximar. Somente o padre Alfredo é um visitante regular: quer levar o homem para se confessar, mas o único assunto que interessa a Celestino é mesmo o esplendor de seu roseiral.",
 
         Capa: 'img/AVisaoDasPlantas.jpg',
         Online:'https://pt.scribd.com/document/546691486/A-visao-das-plantas-Djaimilia-Pereira-de-Almeida'
@@ -127,7 +127,7 @@ const livrosfuvest = {
         Genero:"Romance",
         Editora:"Rocco",
         Ano: 1964,
-        Descricao: "A Paixão Segundo G.H. é um romance escrito por Clarice Lispector, publicado em 1964. A obra narra a experiência de uma mulher que enfrenta uma crise existencial após um encontro inesperado. O livro aborda temas como a identidade, a solidão e a busca por sentido na vida.",
+        Descricao: "Romance original, desprovido das características próprias do gênero, A Paixão Segundo G.H. conta, através de um enredo banal, o pensar e o sentir de G.H., a protagonista-narradora que despede a empregada doméstica e decide fazer uma limpeza geral no quarto de serviço, que ela supõe imundo e repleto de inutilidades. Após recuperar-se da frustração de ter encontrado um quarto limpo e arrumado, G.H. depara-se com uma barata na porta do armário.",
 
         Capa: 'img/APaixaoSegundoGH.jpg',
         Online:'https://www.atelierpaulista.com/wp-content/uploads/2015/07/apaixaosegundogh.pdf'
@@ -140,7 +140,7 @@ const livrosfuvest = {
         Genero:"Romance",
         Editora:"Companhia Das Letras",
         Ano: 1967,
-        Descricao: "Geografia é um romance escrito por Sophia de Mello Breyner Andresen, publicado em 1967. A obra narra a história de uma jovem que busca compreender sua identidade e seu lugar no mundo. O livro aborda temas como a liberdade, a espiritualidade e a busca por sentido na vida.",
+        Descricao: "Geografia é colectânea de poesia da autora portuguesa Sophia de Mello Breyner Andresen, publicada em 1967, na qual foram desenvolvidos os temas da natureza purificadora em oposição à cidade, a casa, a infância e as pessoas das quais a autora se recordava. Em 1963, Sophia viajou pela Grécia e o seu encontro físico com o mundo grego, longamente intuído, inscrever-se-ia explicitamente sobretudo na presente obra.",
         
         Capa: 'img/OCristoCigano_Geografia.jpg',
         Online:'https://pt.scribd.com/document/837241027/O-Cristo-Cigano-e-Geografia-Sophia-de-Mello-Breyner-Andresen'
@@ -156,7 +156,7 @@ const livrosfuvest = {
         Genero:"Literário",
         Editora:"Typographia de F. de Paula Brito.",
         Ano: 1842,
-        Descricao: "Conselhos a Minha Filha é uma obra escrita por Nísia Floresta em 1842, que aborda temas relacionados aos direitos das mulheres e à educação. Nísia Floresta foi uma das primeiras feministas do Brasil e sua obra é considerada pioneira na luta pelos direitos das mulheres no país.",
+        Descricao: "Escrito em formato epistolar, a carta escrita de mãe para filha em tom afetuoso elenca os direitos e virtudes das mulheres no mundo oitocentista, sem deixar de alertar sobre os perigos de relações falsas e a importância das boas escolhas.",
 
         Capa: 'img/ConselhosAMinhaFilha.jpg',
         Online:'https://books.google.pt/books?id=c_ZCAQAAMAAJ&printsec=frontcover&hl=pt-BR&source=gbs_ge_summary_r&cad=0#v=onepage&q&f=false',
@@ -169,7 +169,7 @@ const livrosfuvest = {
         Genero:"Romance",
         Editora:"José Olympio LTDA.",
         Ano: 1932,
-        Descricao: "João Miguel é um romance escrito por Rachel de Queiroz, publicado em 1932. A obra retrata a vida de um jovem nordestino que enfrenta as adversidades impostas pela seca e pela pobreza. O livro aborda temas como a luta pela sobrevivência e a busca por uma vida melhor.",
+        Descricao: "João Miguel é um homem comum. A psicologia do preso é analisada com argúcia por Rachel. A mulher o abandona. Ele se vê só diante do destino que o perturba.",
 
         Capa: 'img/JoaoMiguel.jpg',
         Online:'https://www.academia.edu/36064254/João_miguel'
@@ -185,7 +185,7 @@ const livrosfuvest = {
         Genero:"Romance",
         Editora:"Edições Câmara",
         Ano: 1899,
-        Descricao: "Dom Casmurro é um romance escrito por Machado de Assis, publicado em 1899. A obra narra a história de Bento Santiago, um homem que relembra sua juventude e seu amor por Capitu. O livro aborda temas como a dúvida, a traição e a complexidade das relações humanas.",
+        Descricao: "Romance publicado pela primeira vez em 1899, Dom Casmurro, novo título da série Prazer de Ler da Edições Câmara, apresenta um olhar crítico sobre a sociedade brasileira do século XIX e integra a trilogia realista de Machado de Assis ao lado de Memórias Póstumas de Brás Cubas e Quincas Borba.",
 
         Capa: 'img/DomCasmurro.webp',
         Online:'https://ddcus.org/pdf/summer_reading/11th_grade/Dom_Casmurro-Machado_de_Assis.pdf'
@@ -198,7 +198,7 @@ const livrosfuvest = {
         Genero:"Contos",
         Editora:"Kapulana",
         Ano: 1964,
-        Descricao: "Nós Matamos o Cão Tinhoso é uma coletânea de contos escrita por Luís Bernardo Honwana, publicada em 1964. A obra reúne histórias que exploram a experiência africana, abordando temas como a colonização, a identidade e a luta pela liberdade. Os contos refletem a vivência do autor e sua perspectiva sobre a sociedade moçambicana.",
+        Descricao: "Nós Matamos o Cão Tinhoso! é um clássico da literatura moçambicana, lançado em 1964. É composto por sete contos emocionantes que denunciam a realidade sufocante vivida pelos trabalhadores colonizados e suas famílias durante a opressão colonial portuguesa em Moçambique, sendo a maior parte das narrativas do ponto de vista das crianças. Esta edição também traz o conto 'Rosita, até morrer', nunca antes publicado em livro.",
 
         Capa: 'img/NosMatamosOCaoTinhoso.jpg',
         Online:'https://pt.slideshare.net/slideshow/honwana-luis-bernardo-nos-matamos-o-cao-tinhoso/267142597'
@@ -211,7 +211,7 @@ const livrosfuvest = {
         Genero:"Romance",
         Editora:"Editora Globo",
         Ano: 1970,
-        Descricao: "Incidente em Antares é um romance escrito por Érico Veríssimo, publicado em 1970. A obra narra a história de uma pequena cidade fictícia chamada Antares, onde os mortos voltam à vida para reivindicar seus direitos. O livro aborda temas como a justiça social, a corrupção e a luta pela igualdade.",
+        Descricao: "Numa sexta-feira 13, em 1963, sete pessoas morrem em Antares. Mas os coveiros estão em greve, e os defuntos, insepultos, vagam pela cidade vasculhando a intimidade de parentes e amigos.",
 
         Capa: 'img/IncidenteEmAntares.jpg',
         Online:'https://kbook.com.br/wp-content/files_mf/erico_incidente_em_antares.pdf'
